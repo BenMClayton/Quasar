9d1a1e01fc098dce3499b1d816f4ff1ff017b0a2
BenMClayton <ben.m.clayton.2003@gmail.com>
2023-08-28 18:37:20 +0100

    Initialised cargo project

---
+++ Quasar/target/debug/Quasar.d
@@ -0,0 +1,1 @@
+C:\Users\bencl\OneDrive\Desktop\github\Quasar\Quasar\target\debug\Quasar.exe: C:\Users\bencl\OneDrive\Desktop\github\Quasar\Quasar\src\main.rs
---
+++ Quasar/target/debug/deps/Quasar.d
@@ -0,0 +1,5 @@
+C:\Users\bencl\OneDrive\Desktop\github\Quasar\Quasar\target\debug\deps\Quasar.exe: src\main.rs
+
+C:\Users\bencl\OneDrive\Desktop\github\Quasar\Quasar\target\debug\deps\Quasar.d: src\main.rs
+
+src\main.rs:
